4c9fb0eed770e00324bf59ad5196a3282cd31402
Torsten Pfützenreuter <torsten.pfuetzenreuter@iosb-ast.fraunhofer.de>
2022-10-13 12:58:10 +0200

    Windows: added CPack support Non-Windows OS: enabled shared library and disabled test modules

--- python.cmake
+++ python.cmake
@@ -4,11 +4,24 @@ if(NOT python_tag)
   string(JSON python_tag GET ${json_meta} python tag)
 endif()
 
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(BUILD_LAYOUT_OPTIONS "-d")
+endif()
+
 if(WIN32)
   # https://pythondev.readthedocs.io/windows.html
 
   if(NOT MSVC)
     message(FATAL_ERROR "On Windows, Python is available from Microsoft Store. Python building on Windows requires Visual Studio.")
+  endif()
+  # PCBUILD may contain binaries in win32 / amd64 / arm32 / arm64 subdirs
+  # Todo: add ARM support
+  if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    # 64 bits
+    set(BINARY_DIR PCbuild/amd64/)
+  elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
+    # 32 bits
+    set(BINARY_DIR PCbuild/x86/)
   endif()
 
   ExternalProject_Add(python
@@ -16,13 +29,33 @@ if(WIN32)
   GIT_TAG ${python_tag}
   GIT_SHALLOW true
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND <SOURCE_DIR>/PCBuild/build.bat
-  INSTALL_COMMAND ""
+  BUILD_COMMAND <SOURCE_DIR>/PCBuild/build.bat ${BUILD_LAYOUT_OPTIONS}
+  INSTALL_COMMAND <SOURCE_DIR>/python.bat <SOURCE_DIR>/PC/layout/ ${BUILD_LAYOUT_OPTIONS} -s <SOURCE_DIR> -b <SOURCE_DIR>/${BINARY_DIR} -vv --copy ${CMAKE_CURRENT_BINARY_DIR}/install --preset-default
   TEST_COMMAND ""
   CONFIGURE_HANDLED_BY_BUILD ON
   INACTIVITY_TIMEOUT 60
   )
+  # setup install components
+  
+  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/install/ DESTINATION python COMPONENT runtime PATTERN "*.pdb" EXCLUDE PATTERN "include" EXCLUDE PATTERN "libs" EXCLUDE EXCLUDE PATTERN "tcl" EXCLUDE)
+  
+  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/install/include DESTINATION python COMPONENT develop)
+  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/install/libs DESTINATION python COMPONENT develop)
+  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/install/tcl DESTINATION python COMPONENT develop)
+  
+  # package all components
+  get_cmake_property(CPACK_COMPONENTS_ALL COMPONENTS)
+  SET(CPACK_ARCHIVE_COMPONENT_INSTALL ON) 
+  
+  SET(CPACK_GENERATOR "TGZ")
+  SET(CPACK_INCLUDE_TOPLEVEL_DIRECTORY 0)
+  # determine version from patchlevel.h
+  # retrieve version from header file
+  file (STRINGS "${CMAKE_CURRENT_BINARY_DIR}/install/include/patchlevel.h" version REGEX "^#define[ \t]+PY_VERSION[ \t]+\"[^\"]+\"")
+  string(REGEX REPLACE "^#define[ \t]+PY_VERSION[ \t]+\"([^\"]+)\".*" "\\1" version "${version}")
+  string(TOLOWER "python-${version}-${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}-${CMAKE_BUILD_TYPE}" CPACK_PACKAGE_FILE_NAME)
 
+  include(cpack)
 else()
   # Linux prereqs: https://devguide.python.org/setup/#linux
 
@@ -40,9 +73,10 @@ else()
   --prefix=${CMAKE_INSTALL_PREFIX}
   CC=${CC}
   --with-system-expat
+  --disable-test-modules
   )
   if(CMAKE_BUILD_TYPE STREQUAL "Release")
-    list(APPEND python_args --enable-optimizations)
+    list(APPEND python_args --enable-optimizations --enable-shared)
   endif()
 
   set(python_cflags "${CMAKE_C_FLAGS}")
